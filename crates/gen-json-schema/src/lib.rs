b4233b576c2013d664d525ef04de2a7764ed3c87
Willem Wyndham <wyndham@cs.unc.edu>
2022-06-20 12:43:05 -0400

    feat(JSON): initial work

---
+++ crates/gen-json-schema/src/lib.rs
@@ -0,0 +1,460 @@
+use std::{mem, ops::Deref};
+
+use heck::*;
+pub use valico::json_schema::{
+    builder::{Builder, Dependencies},
+    PrimitiveType,
+};
+use wit_bindgen_gen_core::{wit_parser, Direction, Files, Generator, Source};
+use wit_parser::*;
+
+pub mod schema;
+
+pub struct JSONSchema {
+    deps: Deps,
+}
+
+pub struct Deps(Dependencies);
+
+impl Default for Deps {
+    fn default() -> Self {
+        Self(Dependencies::new())
+    }
+}
+
+impl Deref for Deps {
+    type Target = Dependencies;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+trait AddDep {
+    fn add_dep(&mut self, s: &str);
+}
+
+impl AddDep for Builder {
+    fn add_dep(&mut self, s: &str) {
+        self.ref_(&format!("#/dependencies/{s}"))
+    }
+}
+
+#[derive(Default, Debug, Clone)]
+#[cfg_attr(feature = "structopt", derive(structopt::StructOpt))]
+pub struct Opts {
+    // ...
+}
+
+impl Opts {
+    pub fn build(&self) -> JSONSchema {
+        let mut r = JSONSchema::new();
+        r
+    }
+}
+
+fn unwrap_option<'a>(iface: &'a Interface, ty: &'a Type) -> Option<&'a Type> {
+    match ty {
+        Type::Id(id) => {
+            let ty = &iface.types[*id];
+            match &ty.kind {
+                TypeDefKind::Option(t) => Some(&t),
+                _ => None,
+            }
+        }
+        _ => None,
+    }
+}
+
+impl JSONSchema {
+    pub fn new() -> Self {
+        Self {
+            deps: Deps::default(),
+        }
+    }
+
+    // fn
+
+    fn print_ty(&mut self, iface: &Interface, ty: &Type, builder: &mut Builder) {
+        match ty {
+            Type::Unit => builder.add_dep("unit"),
+            Type::Bool => builder.boolean(),
+            Type::U8 => builder.add_dep("u8"),
+            Type::S8 => builder.add_dep("i8"),
+            Type::U16 => builder.add_dep("u16"),
+            Type::S16 => builder.add_dep("i16"),
+            Type::U32 => builder.add_dep("u32"),
+            Type::S32 => builder.add_dep("i32"),
+            Type::U64 => builder.add_dep("u64"),
+            Type::S64 => builder.add_dep("i64"),
+            Type::Float32 => builder.add_dep("f32"),
+            Type::Float64 => builder.add_dep("f64"),
+            Type::Char => builder.add_dep("char"),
+            Type::String => builder.string(),
+            Type::Handle(id) => {
+                // self.src.push_str("handle<");
+                // self.src.push_str(&iface.resources[*id].name);
+                // self.src.push_str(">");
+            }
+            Type::Id(id) => {
+                let ty = &iface.types[*id];
+                // if !skip_name {
+                //     if let Some(name) = &ty.name {
+                //         self.src.push_str("[`");
+                //         self.src.push_str(name);
+                //         self.src.push_str("`](#");
+                //         self.src.push_str(&name.to_snake_case());
+                //         self.src.push_str(")");
+                //         return;
+                //     }
+                // }
+                match &ty.kind {
+                    TypeDefKind::Type(t) => self.print_ty(iface, t, builder),
+                    TypeDefKind::Tuple(t) => self.print_tuple(iface, t, builder),
+                    TypeDefKind::Record(_)
+                    | TypeDefKind::Flags(_)
+                    | TypeDefKind::Enum(_)
+                    | TypeDefKind::Variant(_)
+                    | TypeDefKind::Union(_) => {
+                        unreachable!()
+                    }
+                    TypeDefKind::Option(t) => {
+                        self.print_ty(iface, t, builder);
+                        builder.null()
+                    }
+                    TypeDefKind::Expected(e) => builder.one_of(|s| {
+                        s.push(|s| {
+                            self.print_ty(iface, &e.ok, s);
+                            self.print_ty(iface, &e.err, s);
+                        })
+                    }),
+                    TypeDefKind::List(t) => {
+                        builder.array();
+                        builder.items_array(|s| s.push(|s| self.print_ty(iface, t, s)));
+                    }
+                    TypeDefKind::Stream(s) => {
+                        // self.src.push_str("stream<");
+                        // self.print_ty(iface, &s.element, false);
+                        // self.src.push_str(", ");
+                        // self.print_ty(iface, &s.end, false);
+                        // self.src.push_str(">");
+                    }
+                }
+            }
+        }
+    }
+
+    fn print_tuple(&mut self, iface: &Interface, tuple: &Tuple, builder: &mut Builder) {
+        builder.array();
+        let len = tuple.types.len() as u64;
+        builder.max_length(len);
+        builder.min_length(len);
+        builder.items_array(|s| {
+            for ty in tuple.types.iter() {
+                s.push(|b| self.print_ty(iface, ty, b));
+            }
+        });
+    }
+
+    fn docs(&mut self, docs: &Docs, builder: &mut Builder) {
+        let docs = match &docs.contents {
+            Some(docs) => docs,
+            None => return,
+        };
+        builder.desc(docs)
+    }
+
+    //     fn print_type_header(&mut self, name: &str) {
+    //         if self.types == 0 {
+    //             self.src.push_str("# Types\n\n");
+    //         }
+    //         self.types += 1;
+    //         self.src.push_str(&format!(
+    //             "## <a href=\"#{}\" name=\"{0}\"></a> `{}`: ",
+    //             name.to_snake_case(),
+    //             name,
+    //         ));
+    //         self.hrefs
+    //             .insert(name.to_string(), format!("#{}", name.to_snake_case()));
+    //     }
+
+    //     fn print_type_info(&mut self, ty: TypeId, docs: &Docs) {
+    //         self.docs(docs);
+    //         self.src.push_str("\n");
+    //         self.src
+    //             .push_str(&format!("Size: {}, ", self.sizes.size(&Type::Id(ty))));
+    //         self.src
+    //             .push_str(&format!("Alignment: {}\n", self.sizes.align(&Type::Id(ty))));
+    //     }
+}
+
+impl Generator for JSONSchema {
+    fn preprocess_one(&mut self, iface: &Interface, _dir: Direction) {
+        // self.sizes.fill(iface);
+        // schema::add_primitives(&mut self.deps.0);
+    }
+
+    fn type_record(
+        &mut self,
+        iface: &Interface,
+        id: TypeId,
+        name: &str,
+        record: &Record,
+        docs: &Docs,
+    ) {
+        let mut d = mem::take(&mut self.deps);
+        d.0.schema(name, |builder| {
+            // builder.id(name);
+            builder.object();
+            builder.additional_properties(false);
+            self.docs(docs, builder);
+            let mut req = vec![];
+            builder.properties(|hash| {
+                for Field { docs, name, ty } in record.fields.iter() {
+                    hash.insert(name, |builder| {
+                        let ty = unwrap_option(iface, ty).unwrap_or_else(|| {
+                            req.push(name.to_string());
+                            ty
+                        });
+                        self.print_ty(iface, ty, builder);
+                        self.docs(docs, builder)
+                    })
+                }
+            });
+            builder.required(req)
+        });
+        self.deps = d;
+    }
+
+    fn type_tuple(
+        &mut self,
+        iface: &Interface,
+        id: TypeId,
+        name: &str,
+        tuple: &Tuple,
+        docs: &Docs,
+    ) {
+        let mut d = mem::take(&mut self.deps);
+        d.0.schema(name, |builder| {
+            self.print_tuple(iface, tuple, builder);
+            self.docs(docs, builder);
+        });
+        self.deps = d;
+    }
+
+    fn type_flags(
+        &mut self,
+        _iface: &Interface,
+        id: TypeId,
+        name: &str,
+        flags: &Flags,
+        docs: &Docs,
+    ) {
+    }
+
+    fn type_variant(
+        &mut self,
+        iface: &Interface,
+        id: TypeId,
+        name: &str,
+        variant: &Variant,
+        docs: &Docs,
+    ) {
+    }
+
+    fn type_union(
+        &mut self,
+        iface: &Interface,
+        id: TypeId,
+        name: &str,
+        union: &Union,
+        docs: &Docs,
+    ) {
+    }
+
+    fn type_enum(&mut self, _iface: &Interface, id: TypeId, name: &str, enum_: &Enum, docs: &Docs) {
+    }
+
+    fn type_option(
+        &mut self,
+        iface: &Interface,
+        id: TypeId,
+        name: &str,
+        payload: &Type,
+        docs: &Docs,
+    ) {
+    }
+
+    fn type_expected(
+        &mut self,
+        iface: &Interface,
+        id: TypeId,
+        name: &str,
+        expected: &Expected,
+        docs: &Docs,
+    ) {
+        // self.type_alias(iface, id,name, ty, docs)
+    }
+
+    fn type_resource(&mut self, iface: &Interface, ty: ResourceId) {
+        drop((iface, ty));
+    }
+
+    fn type_alias(&mut self, iface: &Interface, id: TypeId, name: &str, ty: &Type, docs: &Docs) {
+        let mut d = mem::take(&mut self.deps);
+        d.0.schema(name, |builder| {
+            self.print_ty(iface, ty, builder);
+            self.docs(docs, builder)
+        });
+        self.deps = d;
+    }
+
+    fn type_list(&mut self, iface: &Interface, id: TypeId, name: &str, _ty: &Type, docs: &Docs) {
+        self.type_alias(iface, id, name, &Type::Id(id), docs);
+    }
+
+    fn type_builtin(&mut self, iface: &Interface, id: TypeId, name: &str, ty: &Type, docs: &Docs) {
+        self.type_alias(iface, id, name, ty, docs)
+    }
+
+    fn import(&mut self, iface: &Interface, func: &Function) {}
+
+    fn export(&mut self, iface: &Interface, func: &Function) {
+        self.import(iface, func);
+    }
+
+    fn finish_one(&mut self, _iface: &Interface, files: &mut Files) {
+        let mut builder = valico::json_schema::Builder::new();
+        // builder.dependencies(build)
+        builder.schema("http://json-schema.org/draft-07/schema#");
+        builder.dependencies(|deps| {
+            let d = mem::take(&mut self.deps).0;
+            let _ = mem::replace(deps, d);
+        });
+
+        println!("{}", builder.into_json());
+
+        // let parser = Parser::new(&self.src);
+        // let mut events = Vec::new();
+        // for event in parser {
+        //     if let Event::Code(code) = &event {
+        //         if let Some(dst) = self.hrefs.get(code.as_ref()) {
+        //             let tag = Tag::Link(LinkType::Inline, dst.as_str().into(), "".into());
+        //             events.push(Event::Start(tag.clone()));
+        //             events.push(event.clone());
+        //             events.push(Event::End(tag));
+        //             continue;
+        //         }
+        //     }
+        //     events.push(event);
+        // }
+        // let mut html_output = String::new();
+        // html::push_html(&mut html_output, events.into_iter());
+
+        // files.push("bindings.md", self.src.as_bytes());
+        // files.push("bindings.html", html_output.as_bytes());
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::JSONSchema;
+    use crate::schema::add_primitives;
+    use anyhow::Result;
+    use valico::json_schema::PrimitiveType;
+    use wit_bindgen_gen_core::{wit_parser::Interface, Direction, Files, Generator};
+
+    fn parse_wit_str(s: &str) -> Result<Interface> {
+        Interface::parse("a", s)
+    }
+
+    fn gen_interface(i: Interface) {
+        let mut schema = JSONSchema::new();
+        println!("{:#?}", i);
+        let imports = vec![i];
+        schema.preprocess_all(&imports, &[]);
+        let mut files = Files::default();
+        schema.generate_one(&imports[0], Direction::Import, &mut files);
+        schema.finish_all(&mut files);
+    }
+
+    fn get_str(s: &str) {
+        gen_interface(parse_wit_str(s).expect(s));
+    }
+
+    #[test]
+    fn simple() {
+        get_str(
+            r#"
+/// This is a doc string
+type u128 = string
+      "#,
+        );
+    }
+
+    #[test]
+    fn record() {
+        get_str(
+            r#"
+/// This is a doc string
+record foo {
+  req-field: string,
+  optional: option<u32>,
+}
+    "#,
+        );
+    }
+
+    #[test]
+    fn list() {
+        get_str(
+            r#"
+/// This is a doc string
+type bytes = list<u8>
+      "#,
+        );
+    }
+
+    #[test]
+    fn tuple() {
+        get_str(
+            r#"
+/// This is a doc string
+type t = tuple<u8, string, option<bool>>
+      "#,
+        );
+    }
+
+    #[test]
+    fn option() {
+      get_str(
+          r#"
+struct foo {}
+
+/// This is a doc string
+type t = option<foo>
+    "#,
+      );
+  }
+
+    #[test]
+    fn builder() {
+        let mut builder = valico::json_schema::Builder::new();
+        // builder.dependencies(build)
+        // builder.schema("https://json");
+        builder.ref_("#/dependencies/Balance");
+        builder.dependencies(|deps| {
+            deps.schema("Balance", |builder| {
+                builder.desc("balance is a type");
+                builder.ref_("#/dependencies/U128");
+            });
+            deps.schema("U128", |b| {
+                b.desc("String rep of u128");
+                b.pattern("^[0-9]+");
+                b.type_(PrimitiveType::String)
+            });
+            add_primitives(deps);
+        });
+        println!("{}", builder.into_json());
+    }
+}
---
+++ crates/gen-json-schema/src/schema.rs
@@ -0,0 +1,207 @@
+use valico::json_schema::{builder::Dependencies, PrimitiveType};
+use PrimitiveType::{Integer, Number};
+
+pub fn add_primitives(deps: &mut Dependencies) {
+
+    deps.schema("u64", |b| {
+        b.type_(Integer);
+        b.minimum(0f64);
+        b.maximum(18446744073709551615f64);
+    });
+    deps.schema("i64", |b| {
+        b.type_(Integer);
+        b.minimum(-9223372036854775808f64);
+        b.maximum(9223372036854775807f64);
+    });
+    deps.schema("u8", |b| {
+        b.type_(Integer);
+        b.minimum(0f64);
+        b.maximum(255f64);
+    });
+    deps.schema("i8", |b| {
+        b.type_(Integer);
+        b.minimum(-128f64);
+        b.maximum(127f64);
+    });
+    deps.schema("u16", |b| {
+        b.type_(Integer);
+        b.minimum(0f64);
+        b.maximum(65535f64);
+    });
+    deps.schema("i16", |b| {
+        b.type_(Integer);
+        b.minimum(-32768f64);
+        b.maximum(32767f64);
+    });
+    deps.schema("u32", |b| {
+        b.type_(Integer);
+        b.minimum(0f64);
+        b.maximum(4294967295f64);
+    });
+    deps.schema("usize", |b| {
+        b.type_(Integer);
+        b.minimum(0f64);
+        b.maximum(4294967295f64);
+    });
+    deps.schema("i32", |b| {
+        b.type_(Integer);
+        b.minimum(-2147483648f64);
+        b.maximum(2147483647f64);
+    });
+
+    deps.schema("f32", |b| {
+        b.type_(Number);
+        b.minimum(-3.40282347E+38);
+        b.maximum(3.40282347E+38);
+    });
+
+    deps.schema("f64", |b| {
+        b.type_(Number);
+        b.minimum(-1.7976931348623157E+308);
+        b.maximum(1.7976931348623157E+308);
+    });
+
+    deps.schema("unit",|b|{});
+
+    deps.schema("char",|b|{
+      b.string();
+      b.max_length(1);
+    });
+
+}
+
+
+/*
+{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "$ref": "#/definitions/Balance",
+  "definitions": {
+    "Balance": {
+      "$ref": "#/definitions/U128",
+      "description": "Balance is a type for storing amounts of tokens, specified in yoctoNEAR."
+    },
+    "CallOptions": {
+      "additionalProperties": false,
+      "properties": {
+        "attachedDeposit": {
+          "$ref": "#/definitions/Balance",
+          "default": "0",
+          "description": "Units in yoctoNear"
+        },
+        "gas": {
+          "default": "30000000000000",
+          "description": "Units in gas",
+          "pattern": "[0-9]+",
+          "type": "string"
+        }
+      },
+      "type": "object"
+    },
+    "CurrentVersion": {
+      "additionalProperties": false,
+      "contractMethod": "view",
+      "properties": {
+        "args": {
+          "additionalProperties": false,
+          "type": "object"
+        }
+      },
+      "required": [
+        "args"
+      ],
+      "type": "object"
+    },
+    "CurrentVersion__Result": {
+      "type": "string"
+    },
+    "Fetch": {
+      "additionalProperties": false,
+      "contractMethod": "view",
+      "properties": {
+        "args": {
+          "additionalProperties": false,
+          "type": "object"
+        }
+      },
+      "required": [
+        "args"
+      ],
+      "type": "object"
+    },
+    "Fetch__Result": {
+      "type": "null"
+    },
+    "Major": {
+      "additionalProperties": false,
+      "contractMethod": "change",
+      "description": "Breaking change",
+      "properties": {
+        "args": {
+          "additionalProperties": false,
+          "type": "object"
+        },
+        "options": {
+          "$ref": "#/definitions/CallOptions"
+        }
+      },
+      "required": [
+        "args",
+        "options"
+      ],
+      "type": "object"
+    },
+    "Major__Result": {
+      "type": "null"
+    },
+    "Minor": {
+      "additionalProperties": false,
+      "contractMethod": "change",
+      "description": "Non-breaking feature",
+      "properties": {
+        "args": {
+          "additionalProperties": false,
+          "type": "object"
+        },
+        "options": {
+          "$ref": "#/definitions/CallOptions"
+        }
+      },
+      "required": [
+        "args",
+        "options"
+      ],
+      "type": "object"
+    },
+    "Minor__Result": {
+      "type": "null"
+    },
+    "Patch": {
+      "additionalProperties": false,
+      "contractMethod": "change",
+      "description": "Non-breaking fix",
+      "properties": {
+        "args": {
+          "additionalProperties": false,
+          "type": "object"
+        },
+        "options": {
+          "$ref": "#/definitions/CallOptions"
+        }
+      },
+      "required": [
+        "args",
+        "options"
+      ],
+      "type": "object"
+    },
+    "Patch__Result": {
+      "type": "null"
+    },
+    "U128": {
+      "description": "String representation of a u128-bit integer",
+      "pattern": "^[0-9]+$",
+      "type": "string"
+    }
+  }
+}
+*/
